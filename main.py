47723ca62760a5c0417a2fccad009c4a0fe7ca8d
mscschliemann <46246023+mscschliemann@users.noreply.github.com>
2021-03-30 22:08:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def create_keyfile_dict():
         "type": os.environ.get("SHEET_TYPE"),
         "project_id": os.environ.get("SHEET_PROJECT_ID"),
         "private_key_id": os.environ.get("SHEET_PRIVATE_KEY_ID"),
-        "private_key": os.environ.get("SHEET_PRIVATE_KEY"),
+        "private_key": os.environ.get("SHEET_PRIVATE_KEY").replace('\\n', '\n'),
         "client_email": os.environ.get("SHEET_CLIENT_EMAIL"),
         "client_id": os.environ.get("SHEET_CLIENT_ID"),
         "auth_uri": os.environ.get("SHEET_AUTH_URI"),
@@ -81,7 +81,6 @@ lang={lang}'
 
     new_row = [str(datetime.datetime.now()), max_temp, min_temp, loc, status]
     worksheet.append_row(new_row)
-
 
 
 
