5b2c8dff75160c043373189c1269c8d18fcb2935
mscschliemann <46246023+mscschliemann@users.noreply.github.com>
2021-03-30 20:35:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests as req
 import re, os, json
 import datetime
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+#from boto.s3.connection import S3Connection
 
 data = {'time': [],
         'max': [],
@@ -10,6 +12,25 @@ data = {'time': [],
 uris = ['https://www.wetter.de/deutschland/wetter-berlin-18228265.html?q=berlin',
         'https://www.wetter.de/deutschland/wetter-stuttgart-18224193.html?q=stuttgart',
         'https://www.wetter.de/usa/wetter-springfield-18820972/wetterprognose.html?q=Springfield,%20Missouri,%20Vereinigte%20Staaten%20von%20Amerika']
+
+
+def create_keyfile_dict():
+    variables_keys = {
+        "type": os.environ.get("SHEET_TYPE"),
+        "project_id": os.environ.get("SHEET_PROJECT_ID"),
+        "private_key_id": os.environ.get("SHEET_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("SHEET_PRIVATE_KEY"),
+        "client_email": os.environ.get("SHEET_CLIENT_EMAIL"),
+        "client_id": os.environ.get("SHEET_CLIENT_ID"),
+        "auth_uri": os.environ.get("SHEET_AUTH_URI"),
+        "token_uri": os.environ.get("SHEET_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get("SHEET_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.environ.get("SHEET_CLIENT_X509_CERT_URL")
+    }
+    return variables_keys
+
+print(create_keyfile_dict())
+
 
 for uri in uris:
     html = req.get(uri)
@@ -47,12 +68,15 @@ lang={lang}'
     print(f"status: {status}")
     print()
 
-    gc = gspread.service_account()
-    sh = gc.open("data")
-    worksheet = sh.get_worksheet(0)
-
-    new_row = [str(datetime.datetime.now()), max_temp, min_temp, loc, status]
-    worksheet.append_row(new_row)
+##
+##    credentials = ServiceAccountCredentials.from_json_keyfile_dict(google_api_key_file, scope)
+##
+##    gc = gspread.service_account()
+##    sh = gc.open("data")
+##    worksheet = sh.get_worksheet(0)
+##
+##    new_row = [str(datetime.datetime.now()), max_temp, min_temp, loc, status]
+##    worksheet.append_row(new_row)
 
 
 
